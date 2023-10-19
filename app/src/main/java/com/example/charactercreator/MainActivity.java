9898f0174ab5b9ac474e4ee406141e9caf57346a
GitRylan1 <aklomp@css.edu>
2023-10-19 11:30:27 -0500

    Sprint 1

--- app/src/main/java/com/example/charactercreator/MainActivity.java
+++ app/src/main/java/com/example/charactercreator/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.charactercreator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+Button buttonRandom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
