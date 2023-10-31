cc1ad73aa2705e29ef2948b8f9970361e4ce255c
GitRylan1 <aklomp@css.edu>
2023-10-30 21:23:40 -0500

    Sprint 3

--- app/src/main/java/com/example/charactercreator/MainActivity.java
+++ app/src/main/java/com/example/charactercreator/MainActivity.java
@@ -5,19 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
-
+import com.squareup.picasso.Picasso;
 import java.io.InputStream;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-Button buttonRandom;
+Button buttonSave;
+Spinner spinnerRace;
 EditText editTextName;
-EditText editTextRace;
 EditText editTextAge;
 EditText editTextGender;
 EditText editTextAlign;
@@ -36,13 +39,47 @@ ImageView imageViewLooks;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupbuttonRandom();
+        imageViewLooks = findViewById(R.id.imageViewLooks);
+
+        String imageUrl = "https://www.dndbeyond.com/avatars/thumbnails/6/254/420/618/636271781394265550.png";
+
+        Picasso.get().load(imageUrl).into(imageViewLooks);
+
+        setupbuttonSave();
+        setupSpinnerRace();
     }
 
-    private void setupbuttonRandom() {
-        buttonRandom = findViewById(R.id.buttonRandom);
 
-        buttonRandom.setOnClickListener(new View.OnClickListener()
+    private void callArray() {
+        String raceImage[] = getResources().getStringArray(R.array.raceimage_array);
+    }
+
+    private void setupSpinnerRace() {
+        spinnerRace = (Spinner) findViewById(R.id.spinnerRace);
+        String raceName[] = getResources().getStringArray(R.array.racename_array);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, raceName);
+        itemsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerRace.setAdapter(itemsAdapter);
+
+        spinnerRace.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+
+    private void setupbuttonSave() {
+        buttonSave = findViewById(R.id.buttonSave);
+
+        buttonSave.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
